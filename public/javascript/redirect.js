d1e770bacfa63580f9edbb1fbb1873fd42d157e6
Syed Hassan Raza <itsgardezi@gmail.com>
2020-04-25 02:06:41 +0500

    Update redirect.js

--- public/javascript/redirect.js
+++ public/javascript/redirect.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   setTimeout(function() {
     // eslint-disable-next-line eqeqeq
     if (window.location.href.indexOf('response') != -1) {
-      window.location = 'http://localhost:3001/packages';
+      window.location = 'https://guest-guide-book.netlify.com/packages';
     }
   }, 5000);
 });
